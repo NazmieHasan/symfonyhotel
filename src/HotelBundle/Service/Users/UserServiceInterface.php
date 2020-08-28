575a2c6b22fa847445f5a69082394ec8243a00cf
NazmieHasan <58294359+NazmieHasan@users.noreply.github.com>
2020-08-28 11:19:17 +0300

    Encryption

---
+++ src/HotelBundle/Service/Users/UserServiceInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace HotelBundle\Service\Users;
+
+use HotelBundle\Entity\User;
+
+interface UserServiceInterface
+{
+    public function findOneByEmail(string $email) : ?User;
+    public function save(User $user) : bool;
+    public function update(User $user) : bool;
+    public function findOneById(int $id) : ?User;
+    public function findOne(User $user) : ?User;
+    public function currentUser() : ?User;
+    public function getAll();
+
+}
